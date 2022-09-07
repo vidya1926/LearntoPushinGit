07f065224efd4315cbbf59055dfb0c42433eb894
vidya1926 <vidyar1926@gmail.com>
2022-09-07 17:07:15 +0530

    Initial Commit

---
+++ src/main/java/testcases/Executabletestng.java
@@ -0,0 +1,32 @@
+package testcases;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.testng.TestNG;
+import org.testng.xml.XmlClass;
+import org.testng.xml.XmlSuite;
+import org.testng.xml.XmlTest;
+
+public class Executabletestng {
+
+	public static void main(String[] args) {
+		XmlSuite suite = new XmlSuite();
+		suite.setName("Suite");
+		 
+		XmlTest test = new XmlTest(suite);
+		test.setName("Test");
+		List<XmlClass> classes = new ArrayList<XmlClass>();
+		classes.add(new XmlClass("testcases.Logintestcase"));
+		test.setXmlClasses(classes) ;
+		
+		List<XmlSuite> suites = new ArrayList<XmlSuite>();
+		suites.add(suite);
+		TestNG tng = new TestNG();
+		tng.setXmlSuites(suites);
+		tng.run();
+
+
+	}
+
+}
