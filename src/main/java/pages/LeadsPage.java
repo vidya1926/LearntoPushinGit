443aa9c83d13ecde0c5620ebc10101c963bf7bdd
vidya1926 <83778108+vidya1926@users.noreply.github.com>
2022-09-07 11:38:23 +0530

    Update LeadsPage.java

--- src/main/java/pages/LeadsPage.java
+++ src/main/java/pages/LeadsPage.java
@@ -9,6 +9,7 @@ public class LeadsPage extends ProjectSpecificMethod{
 		driver.findElement(By.linkText("Create Lead")).click();
 		String title=driver.getTitle();
 		System.out.println(title);
+		System.out.println("merge Conflict");
 		
 	}
 }
