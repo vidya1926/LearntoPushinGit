2c3a0f77d0af4c004af2a6e90efb53fa21c50194
vidya1926 <83778108+vidya1926@users.noreply.github.com>
2024-02-13 09:20:37 +0530

    Update ViewLeadPage.java

--- src/main/java/pages/ViewLeadPage.java
+++ src/main/java/pages/ViewLeadPage.java
@@ -7,6 +7,7 @@ import BaseClass.ProjectSpecificMethod;
 public class ViewLeadPage extends ProjectSpecificMethod {
 
 	public void verifyFname() {
+		
 		String text = driver.findElement(By.id("viewLead_firstName_sp")).getText();
 		System.out.println(text);
 		
