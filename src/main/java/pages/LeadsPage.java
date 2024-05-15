a33643845812bd1d0f432c59dddbc0f5dfbab181
vidya1926 <83778108+vidya1926@users.noreply.github.com>
2024-05-15 13:08:26 +0530

    update done in leads page
    removed comment line

--- src/main/java/pages/LeadsPage.java
+++ src/main/java/pages/LeadsPage.java
@@ -9,7 +9,7 @@ public class LeadsPage extends ProjectSpecificMethod{
 		driver.findElement(By.linkText("Create Lead")).click();
 		String title=driver.getTitle();
 		System.out.println(title);
-		//Merge conflicts
+	
 		System.out.println("Learns merge Conflifts");
 
 	}
